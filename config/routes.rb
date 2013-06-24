7469c96f6b57457bab685bacc0f9f60cd62c95ca
Jake Levy <jake.r.levy@gmail.com>
2013-06-24 16:04:13 -0400

    Finish User Sign-up

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
   SampleApp::Application.routes.draw do
-  get "users/new"
+  resources :users
+
   root  to: 'static_pages#home' 
 
   match '/signup', to: 'users#new'
   
-  match '/about',   to: 'static_pages#about'
+  match '/about',   to: 'static_pages#About'
   match '/contact', to: 'static_pages#contact'
   match '/help',    to: 'static_pages#help'
  
---
+++ spec/factories.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+
+	factory :user do
+
+		name "Jake Levy"
+		email "jake.r.levy@gmail.com"
+		password "foobar"
+		password_confirmation "foobar"
+	end
+end
